8c30f3d396b79430c53aa8cd93ae7113aa26cc79
Fredrik Strandin <strandin72@gmail.com>
2020-09-29 11:08:55 +0200

    Add testing project SoccerClub.GraphQL.IntegrationTests

---
+++ test/SoccerClub.GraphQLIntegrationTests/HealthCheckTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SoccerClub.GraphQL.IntegrationTests
+{
+    public class HealthCheckTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly HttpClient _httpClient;
+
+        public HealthCheckTests(WebApplicationFactory<Startup> factory)
+        {
+            _httpClient = factory.CreateDefaultClient();
+        }
+
+        [Fact]
+        public async Task HealthCheck_ReturnsOk()
+        {
+            var response = await _httpClient.GetAsync("/healthcheck");
+
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
